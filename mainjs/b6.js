11353b4c8aae24d540494aab4c7e7a6b94bc29f3
royhung <lemanhhung1555@gmail.com>
2025-07-06 16:26:05 +0700

    Numbers vs ep kieu numbers

---
+++ mainjs/b6.js
@@ -0,0 +1,8 @@
+console.log('Hello world from HTML'.toLocaleUpperCase);
+let a = 5; // int, float, double, lonh
+let b = 6;
+let c = 6.9;
+let d = '5';
+let e = +d; // let e = number(d)
+console.log('e value: ', e, 'type: ', typeof e);
+console.log(a === e, 'a: ', typeof a, 'd: ', typeof d);
