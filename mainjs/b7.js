97ec339f2bd931b418a08ce3159aa90e5a9e676f
royhung <lemanhhung1555@gmail.com>
2025-07-07 10:32:19 +0700

    kiểu dữ liệu obj

--- mainjs/b7.js
+++ mainjs/b7.js
@@ -1,9 +1,24 @@
-let a = 4 + 5 + 'a';
-let b = 'a' + 4 + 5;
+let x = {};
+let y = 'Eric';
 
-console.log(4 + 5 + 'a'); // 9a
-console.log('a' + 4 + 5); // a45
+// console.log('type of a:', typeof a, ' type of b: ', typeof b);
 
-console.log("Compare a, b: ", a == b);
-console.log("Compare a, b: ", a === b);
+let obj = {
+    'name': 'eric',
+    'address_vn': 'Ha noi'
+};
 
+// Tổng kết:
+// obj[b]: dùng khi bạn muốn truy cập động (bằng giá trị của biến).
+
+// obj.b: dùng khi bạn muốn truy cập trực tiếp theo tên thuộc tính.
+
+let b = 'name';
+obj[b] = 'Hary';
+obj.b = 'Ali';
+
+let c = '';
+
+// console.log('what is your name? ', ` My name's `, obj.name);
+console.log('what is your name? ', ` My name's `, obj['name'], obj);
+console.log('where are you from? ', ` I'm from`, obj.address_vn);
