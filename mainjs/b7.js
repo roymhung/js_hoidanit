6f5806a14f9f2f1023af81bda24c4d74dff4a4de
royhung <lemanhhung1555@gmail.com>
2025-07-07 08:42:25 +0700

    so sanh == vs ===

---
+++ mainjs/b7.js
@@ -0,0 +1,9 @@
+let a = 4 + 5 + 'a';
+let b = 'a' + 4 + 5;
+
+console.log(4 + 5 + 'a'); // 9a
+console.log('a' + 4 + 5); // a45
+
+console.log("Compare a, b: ", a == b);
+console.log("Compare a, b: ", a === b);
+
